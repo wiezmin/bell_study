21c4b82a813690a939befe00a3002c44e452dc3b
m.krasilnikov <wiezmin92@mail.ru>
2019-04-03 00:18:48 +0300

    Init

---
+++ src/test/java/ru/bellintegrator/organizationapi/OrganizationApiApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.bellintegrator.organizationapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrganizationApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
